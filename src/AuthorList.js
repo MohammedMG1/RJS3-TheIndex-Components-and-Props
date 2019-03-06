63496d9320ffae42013fe01b73804de27fbf414c
Mohammed Alotaibi <imohammed.mg@gmail.com>
2019-03-06 11:05:47 +0300

    Mohammed Alotaibi

---
+++ src/AuthorList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import AuthorCard from "./AuthorCard";
+
+class AuthorList extends Component {
+    render() {
+        let authorList = this.props.authors.map(author => <AuthorCard person={author} />);
+        return (
+            <div className="authors">
+                <h3>Authors</h3>
+                <div className="row"> {authorList}</div>
+
+            </div>
+        );
+    }
+}
+export default AuthorList;
